e753b9f68294b6e774f6bb502965f5326fefad15
saeephalke <saeesphalke@gmail.com>
2024-08-15 12:01:44 -0700

    undo's username

--- frontend/src/TaskDisplay.js
+++ frontend/src/TaskDisplay.js
@@ -2,14 +2,17 @@
 import { Tasks } from "./Tasks"
 import { Textbox } from "./Textbox"
 import { Button } from "./Button"
-import ReactDropdown from "react-dropdown"
 import { Link } from "react-router-dom"
+import React, { useContext } from "react"
+import { UserContext } from "./App"
 
 export function TaskDisplay(props){
 
     const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     const years = [2024, 2025, 2026, 2027, 2028]
+
+    const [username, setUsername] = useContext(UserContext)
     
     return(
 
@@ -42,7 +45,7 @@ export function TaskDisplay(props){
 
             <Tasks task="Clean" dueDate = "08-07-2024"></Tasks>
 
-            <Link to='/'><Button text="Log Out"></Button></Link>
+            <Link to='/' onClick={(e) => setUsername("")}><Button text="Log Out"></Button></Link>
         </>
     )
 
